73044e783b273b056d38fa0579cd2d91600ce5ce
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2025-03-06 19:16:06 +0200

    Update compile.sh

--- FTN/compile.sh
+++ FTN/compile.sh
@@ -3,7 +3,7 @@
 #module load rocm
 
 rm *.mod *.o *.i a.out;
-CC -xhip -munsafe-fp-atomics -g -ggdb  --offload-arch=gfx90a -O3 -c cwrappers.c 
+hipcc -munsafe-fp-atomics -g -ggdb  --offload-arch=gfx90a -O3 -c cwrappers.cu
 ftn -g    -eZ -D_MPIF90 -fPIC -h noomp -h flex_mp=intolerant -O0    -lsci_cray -L/opt/rocm-6.0.3/lib -lamdhip64  -lhipblas  main.f90 cwrappers.o 
 
 # srun -p dev-g --gpus-per-node=1  --ntasks-per-node=1 --nodes=1 -c 7     --time=01:00:00 --account=project_462000007 --mem-per-cpu=80M ./a.out 
