66e7792dd245e3f9ae2ea4bc1514558e1803129e
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2025-03-06 19:19:55 +0200

    Update main.cu

--- C/main.cu
+++ C/main.cu
@@ -1,6 +1,7 @@
-//module load LUMI/24.03
+//module load LUMI
 //module load partition/G
-//module load rocm
+//module use  /appl/local/containers/test-modules  
+//module load rocm/6.2.2
 // hipcc -munsafe-fp-atomics --offload-arch=gfx90a -O3 main.cu -o hip.out
 #include <stdio.h>
 #include <stdlib.h>
